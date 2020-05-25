f3672b5d03ad6be38d58e5b3ccea739f510ec113
Celina Wierzbicka <celinawierzbicka@gmail.com>
2020-05-25 17:22:38 +0200

    Updates

---
+++ precache-manifest.32f1ba5c0b9f7471c76d123c8286bbe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721a8503d619fc869bfe915955396045",
+    "url": "/VideoBrowser/index.html"
+  },
+  {
+    "revision": "1aaa5862174730541b4f",
+    "url": "/VideoBrowser/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "fd261265b20f23258768",
+    "url": "/VideoBrowser/static/js/2.14624d3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VideoBrowser/static/js/2.14624d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aaa5862174730541b4f",
+    "url": "/VideoBrowser/static/js/main.1c356a7e.chunk.js"
+  },
+  {
+    "revision": "c8b852a55e746885a520",
+    "url": "/VideoBrowser/static/js/runtime-main.dbbdf2f2.js"
+  }
+]);
